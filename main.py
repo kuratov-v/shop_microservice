92a3e6a63d6f4aaac5cb89e93110049993bea361
kuratov-v <kuratovv29@gmail.com>
2021-03-23 15:23:19 +0300

    add mongodb connection settings

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from routers.product import router as product_router
 
 app = FastAPI()
 
 app.include_router(product_router, prefix="/api/product")
+
+
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient("mongodb://localhost:27017")
+    app.mongodb = app.mongodb_client["product"]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
